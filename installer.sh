a55c7791efb321d38e5fb6369b78b374dde8ed5d
Venom24 <93699356+venom-24@users.noreply.github.com>
2026-05-21 23:21:40 -0600

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 apt update && apt upgrade -y
-apt install git wget vim -y
-
+apt install git wget vim figlet -y
+cd ~/infected-msf
+chmod 777 *.sh;ls
+cp -r infected_msf.sh $PREFIX/bin
+cd
 wget https://raw.githubusercontent.com/venom-24/venom-24.github.io/refs/heads/main/installer_repo.sh
 
 chmod 777 installer_repo.sh
 bash installer_repo.sh
 
 apt install apktool zipalign -y 
+sleep 2
+bash infected_msf.sh
 
